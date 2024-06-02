b0316b15309281e3af8a1f3947e56e57daf038cc
Jayshree.Paladiya <jayshree.paladiya@celero.ca>
2024-06-02 13:27:05 -0600

    Initial Commit

---
+++ MealRecipeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealRecipeApp
+//
+//  Created by Jayshree Paladiya on 2024-06-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
